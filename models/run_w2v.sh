d09f58b5a91e6d381cba9827c71d34543b8c7803
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-05-26 18:02:30 +0200

    bash for run wav2vec.py

--- models/run_w2v.sh
+++ models/run_w2v.sh
@@ -2,14 +2,14 @@
 
 #SBATCH --mail-user=phamanh@tnt.uni-hannover.de # only <UserName>@tnt.uni-hannover.de is allowed as mail address
 #SBATCH --mail-type=ALL             # Eine Mail wird bei Job-Start/Ende versendet
-#SBATCH --job-name=khoa_test_slurm      # Name unter dem der Job in der Job-History gespeichert wird
+#SBATCH --job-name=w2v_demo      # Name unter dem der Job in der Job-History gespeichert wird
 #SBATCH --output=/home/phamanh/nobackup/DCASE2024/models/result/w2v-%j.txt   # Logdatei für den merged STDOUT/STDERR output (%j wird durch slurm job-ID ersetzt)
 
-#SBATCH --time=00:10:00             # Maximale Laufzeit des Jobs, bis Slurm diesen abbricht (HH:MM:SS)
+#SBATCH --time=24:00:00             # Maximale Laufzeit des Jobs, bis Slurm diesen abbricht (HH:MM:SS)
 #SBATCH --partition=gpu_normal_stud   # Partition auf der gerechnet werden soll. Ohne Angabe des Parameters wird auf der
                                     #   Default-Partition gerechnet. Es können mehrere angegeben werden, mit Komma getrennt.
 #SBATCH --cpus-per-task=8          # Reservierung von 4 CPUs pro Rechenknoten
-#SBATCH --mem=16G                   # Reservierung von 10GB RAM
+#SBATCH --mem=64G                   # Reservierung von 10GB RAM
 #SBATCH --gres=gpu:1
 
 source /home/phamanh/anaconda3/bin/activate /home/phamanh/anaconda3/envs/dcase
