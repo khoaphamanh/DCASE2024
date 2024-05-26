a92084fc99c5fada7af0a3fc9e2ba2e0e928b3d0
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-05-26 17:57:55 +0200

    add utils

---
+++ utils.py
@@ -0,0 +1,16 @@
+# this file contains the hyperparameter of the model
+
+# general hyperparameters
+seed = 1998
+api_token = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJiODUwOWJmNy05M2UzLTQ2ZDItYjU2MS0yZWMwNGI1NDI5ZjAifQ=="
+
+# wav2vec no hyperparameter tunning
+project = "DCASE2024/wav-test"
+lr_w2v = 5e-4
+emb_w2v = 2048
+batch_size_w2v = 64
+wd_w2v = 1e-4
+epochs_w2v = 100
+
+
+min_rate_w2v = 0.8
