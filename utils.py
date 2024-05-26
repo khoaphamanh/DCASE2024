2b9ad6f8e49001896e2012d55858c4e6a06cb12d
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-05-26 21:59:55 +0200

    add utils model_name

--- utils.py
+++ utils.py
@@ -8,9 +8,9 @@ api_token = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjo
 project = "DCASE2024/wav-test"
 lr_w2v = 5e-4
 emb_w2v = 2048
-batch_size_w2v = 64
+batch_size_w2v = 32
 wd_w2v = 1e-4
 epochs_w2v = 100
-
+model_name_w2v = "facebook/wav2vec2-xls-r-300m"
 
 min_rate_w2v = 0.8
