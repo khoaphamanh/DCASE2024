1fc09fd511845be2adfe25e63d16a422914192c5
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-06-05 17:08:56 +0200

    bash arcface

--- models/script_arcface.sh
+++ models/script_arcface.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --mail-user=phamanh@tnt.uni-hannover.de # only <UserName>@tnt.uni-hannover.de is allowed as mail address
 #SBATCH --mail-type=ALL             # Eine Mail wird bei Job-Start/Ende versendet
-#SBATCH --job-name=w2v_adacos     # Name unter dem der Job in der Job-History gespeichert wird
-#SBATCH --output=/home/phamanh/nobackup/DCASE2024/models/result/w2v_adacos-%j.txt   # Logdatei für den merged STDOUT/STDERR output (%j wird durch slurm job-ID ersetzt)
+#SBATCH --job-name=w2v_knn     # Name unter dem der Job in der Job-History gespeichert wird
+#SBATCH --output=/home/phamanh/nobackup/DCASE2024/models/result/w2v_knn-%j.txt   # Logdatei für den merged STDOUT/STDERR output (%j wird durch slurm job-ID ersetzt)
 
 #SBATCH --time=24:00:00             # Maximale Laufzeit des Jobs, bis Slurm diesen abbricht (HH:MM:SS)
 #SBATCH --partition=gpu_normal_stud   # Partition auf der gerechnet werden soll. Ohne Angabe des Parameters wird auf der
@@ -22,6 +22,7 @@ echo "CPU cores: ${SLURM_JOB_CPUS_PER_NODE}"
 echo "GPUs: ${CUDA_VISIBLE_DEVICES}"
 echo "Memory: ${SLURM_MEM_PER_NODE}" 
 echo "Python environment: $(conda info --envs | grep '*' | sed -e 's/^[ \t*]*//')"
+echo "current work directory: ${PWD}"
 echo 
 
 python3 /home/phamanh/nobackup/DCASE2024/models/arcface_w2v.py
