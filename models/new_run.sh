ba104eec2f7c4850e6e3d9ec06aa07281bfe253b
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-07-15 11:11:53 +0200

    add run bash file

---
+++ models/new_run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --mail-user=phamanh@tnt.uni-hannover.de # only <UserName>@tnt.uni-hannover.de is allowed as mail address
+#SBATCH --mail-type=ALL             # Eine Mail wird bei Job-Start/Ende versendet
+#SBATCH --job-name=new_new_pipeline     # Name unter dem der Job in der Job-History gespeichert wird
+#SBATCH --output=/home/phamanh/nobackup/DCASE2024/models/result/a_new_pipeline-%j.txt   # Logdatei für den merged STDOUT/STDERR output (%j wird durch slurm job-ID ersetzt)
+
+#SBATCH --time=99:00:00             # Maximale Laufzeit des Jobs, bis Slurm diesen abbricht (HH:MM:SS)
+#SBATCH --partition=gpu_long_stud   # Partition auf der gerechnet werden soll. Ohne Angabe des Parameters wird auf der
+                                    #   Default-Partition gerechnet. Es können mehrere angegeben werden, mit Komma getrennt.
+#SBATCH --cpus-per-task=8          # Reservierung von 4 CPUs pro Rechenknoten
+#SBATCH --mem=32G                   # Reservierung von 10GB RAM
+#SBATCH --gres=gpu:1
+
+source /home/phamanh/anaconda3/bin/activate /home/phamanh/anaconda3/envs/dcase
+cd /home/phamanh/nobackup/DCASE2024/models
+
+echo "Job ID: ${SLURM_JOB_ID}"
+echo "Node: ${SLURMD_NODENAME}"
+echo "Partition: ${SLURM_JOB_PARTITION}"
+echo "CPU cores: ${SLURM_JOB_CPUS_PER_NODE}"
+echo "GPUs: ${CUDA_VISIBLE_DEVICES}"
+echo "Memory: ${SLURM_MEM_PER_NODE}" 
+echo "Python environment: $(conda info --envs | grep '*' | sed -e 's/^[ \t*]*//')"
+echo "current work directory: ${PWD}"
+echo 
+
+python /home/phamanh/nobackup/DCASE2024/models/new_new_pipeline.py
