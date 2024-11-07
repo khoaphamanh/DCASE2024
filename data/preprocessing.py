c5a012e35240ad2af08e126a96a99b678b99266f
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-11-07 21:30:09 +0100

    preprocessing

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -4,10 +4,10 @@ from scipy.io import wavfile
 import pandas as pd
 import librosa
 import sys
+import pickle
 
 # add path from data preprocessing in data directory
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import download_data
 
 
 # data preprocessing
@@ -16,15 +16,25 @@ class DataPreprocessing:
 
         # directory information
         self.data_name = data_name
-        self.path_data_preprocessing = os.path.abspath(__file__)
-        self.path_data_directory = os.path.dirname(self.path_data_preprocessing)
+        self.path_preprocessing = os.path.abspath(__file__)
+        self.path_data_directory = os.path.dirname(self.path_preprocessing)
         self.path_main_directory = os.path.dirname(self.path_data_directory)
 
         # data information
         self.path_data_name_directory = os.path.join(
             self.path_main_directory, "data", data_name
         )
-        self.machines = sorted(os.listdir(self.path_data_name_directory), key=str.lower)
+        if not os.path.exists(self.path_data_name_directory):
+            import download_data
+
+        self.machines = sorted(
+            [
+                i
+                for i in os.listdir(self.path_data_name_directory)
+                if os.path.isdir(os.path.join(self.path_data_name_directory, i))
+            ],
+            key=str.lower,
+        )
         self.path_machines = [
             os.path.join(self.path_data_name_directory, i) for i in self.machines
         ]
@@ -43,27 +53,44 @@ class DataPreprocessing:
         self.label_condition_number = {"normal": 0, "anomaly": 1}
 
         # dict, list path
-        # self.path_label_unique = os.path.join(f"{}")
+        self.path_label_unique = os.path.join(
+            self.path_data_directory,
+            self.data_name,
+            "{}_label_unique.csv".format(self.data_name),
+        )
+        # print("self:", self.path_label_unique)
+        self.path_data_timeseries_information = os.path.join(
+            self.path_data_directory,
+            self.data_name,
+            "{}_data_timeseries_information.csv".format(self.data_name),
+        )
+        self.path_train_data = os.path.join(
+            self.path_data_directory,
+            self.data_name,
+            "{}_train_data.pkl".format(self.data_name),
+        )
+        self.path_test_data = os.path.join(
+            self.path_data_directory,
+            self.data_name,
+            "{}_test_data.pkl".format(self.data_name),
+        )
+        # print("self:", self.path_data_timeseries_information)
 
     def read_data(self):
         """
         Read features, labels from .wav files
         """
 
-        # train data
-        train_raw_data = []
-        train_label = []
-
-        # test data
-        test_raw_data = []
-        test_label = []
+        # train test data list
+        train_data = []
+        test_data = []
 
         # dict for data timeseries information with syntax [index,name_ts,label_attributt, label_condition ]
         data_timeseries_information = []
         label_unique = []
 
         # loop type train test
-        idx_data = 0  # index of the each timeseries, from 0 to 999 (total 1000)
+        idx_data = 0  # index of the each timeseries, from 0 to 8399 (total 8400)
 
         for t in self.type_data:
 
@@ -83,6 +110,12 @@ class DataPreprocessing:
                     condition = n_ts_split[4]
                     attribute = "_".join(n_ts_split[6:]).replace(".wav", "")
 
+                    # read .wav file
+                    path_n_ts = os.path.join(
+                        self.path_data_name_directory, machine, t, n_ts
+                    )
+                    fs, ts = wavfile.read(path_n_ts)
+
                     # label attribute as string
                     label_attribute = f"{machine}_{domain}_{attribute}"
 
@@ -95,22 +128,83 @@ class DataPreprocessing:
 
                     # append to data_timeseries_information
                     label_attribute_number = label_unique.index(label_attribute)
+                    path_n_ts = os.path.join(machine, n_ts)
                     data_timeseries_information.append(
-                        [idx_data, n_ts, label_attribute_number, label_condition_number]
+                        [
+                            idx_data,
+                            path_n_ts,
+                            label_attribute_number,
+                            label_condition_number,
+                            len(ts),
+                        ]
                     )
+
+                    # append to data list
+                    ts_lb = [ts, label_attribute_number, label_condition_number]
+                    if t == "train":
+                        train_data.append(ts_lb)
+                    elif t == "test":
+                        test_data.append(ts_lb)
 
                     # next index
                     idx_data = idx_data + 1
 
-                    # break
                 # break
             # break
 
-        # convert
-        label_unique = [[i, label_unique[i]] for i in range(len(label_unique))]
+        # save train test data list
+        with open(self.path_train_data, "wb") as file:
+            pickle.dump(train_data, file)
 
-        print("data_timeseries_information:", data_timeseries_information)
-        print("label_unique:", label_unique)
+        with open(self.path_test_data, "wb") as file:
+            pickle.dump(test_data, file)
+
+        # convert to csv
+        label_unique = [[i, label_unique[i]] for i in range(len(label_unique))]
+        column_names = ["Number", "Attribute"]
+        label_unique = pd.DataFrame(label_unique, columns=column_names)
+
+        column_names = ["Index", "Name", "Attribute", "Condition", "Length"]
+        data_timeseries_information = pd.DataFrame(
+            data_timeseries_information, columns=column_names
+        )
+
+        # save the data frame to data name directory
+        label_unique.to_csv(self.path_label_unique, index=False)
+        data_timeseries_information.to_csv(
+            self.path_data_timeseries_information, index=False
+        )
+
+    def label_unique(self):
+        # load label unique as csv
+        if not os.path.exists(self.path_label_unique):
+            self.read_data()
+        else:
+            label_unique = pd.read_csv(self.path_label_unique)
+
+        return label_unique
+
+    def data_timeseries_information(self):
+        # load data_timeseries_information as csv
+        if not os.path.exists(self.path_data_timeseries_information):
+            self.read_data()
+
+        data_timeseries_information = pd.read_csv(self.path_data_timeseries_information)
+
+        return data_timeseries_information
+
+    def load_data(self):
+        # load data .pkl file as list
+        if not os.path.exists(self.path_train_data) or os.path.exists(
+            self.path_test_data
+        ):
+            self.read_data()
+        with open(self.path_train_data, "rb") as file:
+            train_data = pickle.load(file)
+        with open(self.path_test_data, "rb") as file:
+            test_data = pickle.load(file)
+
+        return train_data, test_data
 
 
 if __name__ == "__main__":
@@ -145,4 +239,17 @@ if __name__ == "__main__":
 
     # duration = data_preprocessing.duration
     # print("duration:", duration)
-    data_preprocessing.read_data()
+
+    # data_preprocessing.read_data()
+
+    label_unique = data_preprocessing.label_unique()
+    print("label_unique:", label_unique)
+
+    data_timeseries_information = data_preprocessing.data_timeseries_information()
+    print("data_timeseries_information:", data_timeseries_information)
+
+    # path_label_unique = data_preprocessing.path_label_unique
+    # print("path_label_unique:", path_label_unique)
+
+    # path_train_data = data_preprocessing.path_train_data
+    # print("path_train_data:", path_train_data)
