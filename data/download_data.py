173f7206284a0fe121ed692093d3702a39e7408d
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-11-08 21:24:44 +0100

    download daat

--- data/download_data.py
+++ data/download_data.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 
 # function to download the zipfile data
-def download_zip(url, output_path):
+def download_zip(url, path_output):
 
     # download zip directory develop
     try:
@@ -14,13 +14,13 @@ def download_zip(url, output_path):
         response.raise_for_status()  # Check if the request was successful
 
         # Save the ZIP file temporarily
-        zip_file_path = "{}.zip".format(output_path.split("/")[-1])
+        zip_file_path = "{}.zip".format(path_output.split("/")[-1])
         with open(zip_file_path, "wb") as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
 
         # Extract the contents of the ZIP file
-        unzip(zip_file_path=zip_file_path, output_path=output_path)
+        unzip(path_zip_file=zip_file_path, path_output=path_output)
 
     except requests.exceptions.RequestException as e:
         print(f"Failed to download ZIP file: {e}")
@@ -29,51 +29,51 @@ def download_zip(url, output_path):
 
 
 # unzip data
-def unzip(zip_file_path, output_path):
+def unzip(path_zip_file, path_output):
     try:
         # Open the ZIP file
-        with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+        with zipfile.ZipFile(path_zip_file, "r") as zip_ref:
             # Extract all files to the specified directory
-            zip_ref.extractall(output_path)
+            zip_ref.extractall(path_output)
             print(
-                f"ZIP file '{zip_file_path}' extracted successfully to '{output_path}'"
+                f"ZIP file '{path_zip_file}' extracted successfully to '{path_output}'"
             )
 
         # Remove the temporary ZIP file
-        os.remove(zip_file_path)
+        os.remove(path_zip_file)
 
     except zipfile.BadZipFile:
-        print(f"Error: '{zip_file_path}' is not a valid ZIP file")
+        print(f"Error: '{path_zip_file}' is not a valid ZIP file")
 
 
 # download data function
 def download_data_unzip(data_name, url=None, data_machine=None):
 
     # data directory path
-    data_directory_path = os.path.dirname(os.path.abspath(__file__))
+    path_data_directory = os.path.dirname(os.path.abspath(__file__))
 
     # data path
-    data_path = os.path.join(data_directory_path, data_name)
+    path_data = os.path.join(path_data_directory, data_name)
 
     # check if exist
-    machine_output_path = [os.path.join(data_path, m) for m in data_machine]
+    machine_output_path = [os.path.join(path_data, m) for m in data_machine]
     check_machine = [os.path.exists(mp) for mp in machine_output_path]
 
     # downlaod the zip
-    if not os.path.exists(data_path) or not all(check_machine):
-        download_zip(url=url, output_path=data_path)
-        machine_zip_path = [os.path.join(data_path, i) for i in os.listdir(data_path)]
+    if not os.path.exists(path_data) or not all(check_machine):
+        download_zip(url=url, path_output=path_data)
+        path_machine_zip = [os.path.join(path_data, i) for i in os.listdir(path_data)]
 
         # unzip each machine
         for mp_zip in tqdm(
-            machine_zip_path,
-            total=len(machine_zip_path),
+            path_machine_zip,
+            total=len(path_machine_zip),
             desc="Unzipping {} machine files".format(data_name),
         ):
-            unzip(zip_file_path=mp_zip, output_path=data_path)
+            unzip(path_zip_file=mp_zip, path_output=path_data)
 
     else:
-        print(f"{data_path} is already downloaded")
+        print(f"{path_data} is already downloaded")
         for mp in machine_output_path:
             print(f"{mp} is already downloaded")
 
